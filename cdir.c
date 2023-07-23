7aed9673c94610d0636f25db78a0c20f96e6ba46
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-07-23 16:52:21 +0000

    added word count function to directory changing source code file

--- cdir.c
+++ cdir.c
@@ -1,6 +1,32 @@
 #include "shell.h"
 
 /**
+  * wcount - counts the number of words in a string
+  * @str: string to count words in
+  * @delim: delimeters separating words
+  *
+  * Return: Number of words in string, -1 on failure
+  */
+int wcount(char *str, char *delim)
+{
+	int count;
+	char *dup;
+
+	dup = str;
+	if (str == NULL)
+	{
+		perror("Invalid String");
+		return (-1);
+	}
+
+	strtok(dup, delim);
+	for (count = 1; strtok(NULL, delim) != NULL; count++)
+		;
+
+	return (count);
+}
+
+/**
  * _cdir - changes current working directory and updates OLDPWD
  * @cmd: command to change working directory
  *
